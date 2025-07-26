90e99184affbfb9122d2fc490863c960abf9d65b
shahfahad111 <xyan4321@gmail>
2025-07-26 12:45:31 +0530

    Inital Commit for Smart Contach Manager

---
+++ src/main/java/com/scm/controller/PageController.java
@@ -0,0 +1,41 @@
+package com.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+
+@Controller
+public class PageController {
+   
+    
+    @RequestMapping("/home")
+    public String home(Model model){
+        System.out.println("Home Controller");
+        model.addAttribute("name",  "Fahad");
+        model.addAttribute("designation", "Senior Software Engineer");
+        model.addAttribute("organization", "BNP Paribas");
+        return "home";
+    }
+
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About Page Loading");
+        return "about";
+    }
+    
+    //services
+
+     @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("Services Page Loading");
+        return "services";
+    }
+}
